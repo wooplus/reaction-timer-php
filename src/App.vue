1a44e675e7103a9ef3fbb0cd34fec35d53966cfa
wooplus <aunglinhtet992@gmail.com>
2023-01-24 18:17:52 -0500

    firsttimer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>How fast can you catch me?</h1>
+  <button @click="play" :disabled="isPlaying">play</button>
+  <div v-if="isPlaying">
+    <Block :delay="delay" @endGame="endGame"/>
+  </div>
+  <div v-if="showResult">
+    <Result :score="score"></Result>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Result from './components/Result'
+import Block from './components/Block'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Result,
+    Block,
+  },
+  data() {
+    return {
+      isPlaying:false,
+      delay:null,
+      score:0,
+      showResult:false
+    }
+  },
+  methods:{
+    play() {
+      this.isPlaying = true
+      this.delay = 2000+Math.random()*3000;
+      this.showResult = false
+    },
+    endGame(score) {
+      this.score= score;
+      this.isPlaying= false;
+      this.showResult = true;
+    }
   }
 }
 </script>
---
+++ src/components/Result.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>The reuslt is {{score}}ms</h1>
+  <div v-if="score<250">
+    <p>wow you are thunder speed</p>
+  </div>
+  <div v-else-if="score>250 && score<500">You click at a normal speed</div>
+  <div v-else-if="score > 500">you are so slow</div>
+</template>
+
+<script>
+export default {
+    props:['score']
+}
+</script>
+
+<style>
+
+</style>
